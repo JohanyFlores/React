ac2288ceed56451e177fe5fa1a3d7e23e9bb37fd
JohanyFlores <jfloresm1@student.obsbusiness.school>
2025-08-11 00:11:26 -0600

    Limpieza de codigo

--- src/spellscard.jsx
+++ src/spellscard.jsx
@@ -1,12 +1,10 @@
 import styles from './spellscard.module.css';
 
-// Importaciones de imágenes
 import magiaIcon from "./assets/icons/classes/bard.png";
 import religionIcon from "./assets/icons/classes/cleric.png";
 import tribuIcon from "./assets/icons/classes/druid.png";
 import aldeaIcon from "./assets/icons/classes/wizard.png";
 
-// Mapa para seleccionar el ícono correcto
 const iconMap = {
   magia: magiaIcon,
   religion: religionIcon,
@@ -14,8 +12,6 @@ const iconMap = {
   aldea: aldeaIcon,
 };
 
-// ÚNICA DEFINICIÓN DEL COMPONENTE
-// Asegúrate de que todas las props que necesitas estén listadas aquí
 export function SpellsCard({ name, type, onSelectSpell, level, range, actionType }) { 
   
   return (
@@ -27,11 +23,14 @@ export function SpellsCard({ name, type, onSelectSpell, level, range, actionType
       />
       <p className={styles.spellName}>{name}</p>
 
-      {/* Muestra la nueva información */}
+      {}
       <div className={styles.spellDetails}>
         <p><strong>Nivel:</strong> {level}</p>
         <p><strong>Acción:</strong> {actionType}</p>
         <p><strong>Rango:</strong> {range}</p>
+        <p><strong>Tipo:</strong> {type}</p>
+
+
       </div>
     </div>
   );
